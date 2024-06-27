838ed1c861e03831b4b372a3aa316db3451e2940
trunganh1100 <trunganh1100@gmail.com>
2024-06-27 18:16:01 +0700

    Test video pipeline

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home TEST udacity :))) </h3>"
+    html = "<h3>Sklearn Prediction Home TEST passed udacity </h3>"
     return html.format(format)
 
 # TO DO:  Log out the prediction value
